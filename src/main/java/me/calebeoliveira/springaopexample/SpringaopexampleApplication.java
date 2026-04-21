f20e5d89f28c830ef0339bb0588b72a91e362747
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-04-21 13:52:21 -0300

    first commit

---
+++ src/main/java/me/calebeoliveira/springaopexample/SpringaopexampleApplication.java
@@ -0,0 +1,13 @@
+package me.calebeoliveira.springaopexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaopexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaopexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/calebeoliveira/springaopexample/SpringaopexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.calebeoliveira.springaopexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaopexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
