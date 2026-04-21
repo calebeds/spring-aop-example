0fa076ed23e2a300a2db57d1b0827f0bd0eb45c2
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-04-21 19:45:45 -0300

    add correlation id with MDC

---
+++ src/main/java/me/calebeoliveira/springaopexample/filter/CorrelationIdFilter.java
@@ -0,0 +1,43 @@
+package me.calebeoliveira.springaopexample.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@Order(1)
+public class CorrelationIdFilter extends OncePerRequestFilter {
+
+    private static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
+    private static final String CORRELATION_ID_KEY = "correlationId";
+
+
+    @Override
+    protected void doFilterInternal(final HttpServletRequest request,
+                                    final HttpServletResponse response,
+                                    final FilterChain filterChain) throws ServletException, IOException {
+        String correlationId = request.getHeader(CORRELATION_ID_KEY);
+
+        if(correlationId == null || correlationId.isEmpty()) {
+            correlationId = UUID.randomUUID().toString();
+        }
+
+        MDC.put(CORRELATION_ID_KEY, correlationId);
+
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
