fdeb9a906ff33c2a3a8d4cf25c025afee875c1cc
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2026-04-22 19:00:19 -0300

    comment test

--- src/test/java/me/calebeoliveira/springaopexample/SpringAOPExampleApplicationTests.java
+++ src/test/java/me/calebeoliveira/springaopexample/SpringAOPExampleApplicationTests.java
@@ -3,7 +3,7 @@ package me.calebeoliveira.springaopexample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringAOPExampleApplicationTests {
 
 	@Test
